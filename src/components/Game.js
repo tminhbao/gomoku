0e70b1c5c9fe0c896454dc5354e6ed8a905d25bc
tminhbao <tminhbao2001@gmail.com>
2022-09-29 10:23:08 +0700

    Rewrite the winning rule to 5 consecutive squares

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,13 +7,13 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null),
+          squares: Array(25).fill(null),
           lastMove: null,
         },
       ],
       stepNumber: 0,
       xIsNext: true,
-      styleSquare: Array(9).fill("square"),
+      styleSquare: Array(25).fill("square"),
       ascend: false,
     };
     this.toggle = this.toggle.bind(this);
@@ -24,7 +24,7 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     var styleSquare = this.state.styleSquare.slice();
-    const lastMove = [i % 3, Math.floor(i / 3)];
+    const lastMove = [i % 5, Math.floor(i / 5)];
 
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -52,6 +52,8 @@ class Game extends React.Component {
     styleSquare[winner[0]] = "win";
     styleSquare[winner[1]] = "win";
     styleSquare[winner[2]] = "win";
+    styleSquare[winner[3]] = "win";
+    styleSquare[winner[4]] = "win";
 
     this.setState({
       styleSquare: styleSquare,
@@ -74,7 +76,7 @@ class Game extends React.Component {
       this.onWin(winner, styleSquare);
     } else {
       this.setState({
-        styleSquare: Array(9).fill("square"),
+        styleSquare: Array(25).fill("square"),
       });
     }
   }
@@ -151,19 +153,39 @@ class Game extends React.Component {
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    // [0, 1, 2],
+    // [3, 4, 5],
+    // [6, 7, 8],
+    // [0, 3, 6],
+    // [1, 4, 7],
+    // [2, 5, 8],
+    // [0, 4, 8],
+    // [2, 4, 6],
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20],
   ];
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return [a, b, c];
+    const [a, b, c, d, e] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c] &&
+      squares[a] == squares[d] &&
+      squares[a] == squares[e]
+    ) {
+      return [a, b, c, d, e];
     }
   }
   return null;
