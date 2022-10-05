af05aa253c84d0be4c9341bd25305741b41e3887
tminhbao <tminhbao2001@gmail.com>
2022-10-05 18:18:39 +0700

    update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,27 +10,6 @@ function Board(props) {
       />
     );
   }
-  // function render() {
-  //   const index = [
-  //     [0, 1, 2, 3, 4],
-  //     [5, 6, 7, 8, 9],
-  //     [10, 11, 12, 13, 14],
-  //     [15, 16, 17, 18, 19],
-  //     [20, 21, 22, 23, 24],
-  //   ];
-  //   const rows = index.map((i) => {
-  //     var cells = i.map((j) => {
-  //       return <label key={j.toString()}>{renderSquare(j)}</label>;
-  //     });
-  //     return (
-  //       <div key={i.toString()} className="board-row">
-  //         {cells}
-  //       </div>
-  //     );
-  //   });
-
-  //   // return <div>{rows}</div>;
-  // }
 
   const index = [
     [0, 1, 2, 3, 4],
@@ -49,7 +28,6 @@ function Board(props) {
       </div>
     );
   });
-
   return <div>{rows}</div>;
 }
 
